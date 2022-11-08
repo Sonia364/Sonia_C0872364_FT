7a7d82adc73166a9e604afed01e3a922562f2f85
Sonia364 <c0872364@mylambton.ca>
2022-11-07 20:05:19 -0500

    Implemented Progress screen and its functionality

--- Sonia_C0872364_FT/ProgressVC.swift
+++ Sonia_C0872364_FT/ProgressVC.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class ProgressVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-   
+    weak var delegate: ViewController?
+    var progressData = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        progressData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        cell.textLabel?.text = progressData[indexPath.row]
+        
+        return cell
+    }
     
 
 }
