169ea0f35fcbd3c033259721cab4eca056f88a2e
Sonia364 <c0872364@mylambton.ca>
2022-11-07 19:00:42 -0500

    Added Main screen - labels and image view

--- Sonia_C0872364_FT/ViewController.swift
+++ Sonia_C0872364_FT/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    @IBOutlet weak var randomNumber: UILabel!
+    @IBOutlet weak var evenNumber: UILabel!
+    @IBOutlet weak var oddNumber: UILabel!
+    @IBOutlet weak var correctIncorrect: UIImageView!
+    
+   
+    
 }
+
 
